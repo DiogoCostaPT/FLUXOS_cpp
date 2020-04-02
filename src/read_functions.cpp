3f2d4d432e23d43d3f7f2c7c52934d205397f361
DiogoCostaPT <1208buckas>
2020-04-01 22:42:21 -0600

    DEM is loaded now as full ESRI ASCII and the modset file has now less parameters because some are retrieved directly from the ESRI file

--- src/read_functions.cpp
+++ src/read_functions.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "read_functions.h"
+#include "common.h"
 
 void read_modset(GlobVar& ds, const std::string& filename, 
                 const std::string& pathfile, unsigned int *print_step, 
@@ -46,48 +47,111 @@ void read_modset(GlobVar& ds, const std::string& filename,
 
 void read_geo(GlobVar& ds,double ks_input,std::ofstream& logFLUXOSfile)
 {
-    unsigned int icol,irow;  
-    float zbp,zbp_corr,zbn,zbs,zbe,zbw;
+    int icol,irow,n;  
+    float zbp,zbp_corr,zbn,zbs,zbe,zbw,temp_float;
     arma::mat filedata; 
-    std::string msg;
-    
-    bool flstatus =  filedata.load(ds.dem_file,arma::raw_ascii);
- 
-    if(flstatus == true) {
-        //for(a=0;a<filedata.col(1).n_elem;a++){
-        for(icol=1;icol<=ds.NCOLS;icol++)
+    std::string msg, temp_str;
+
+    arma::mat zb_raw; 
+    zb_raw = arma::zeros<arma::mat>(ds.MROWS,ds.MCOLS);
+
+    std::string line; //this will contain the data read from the file
+    std::ifstream myfile (ds.dem_file); //opening the file.
+    std::stringstream str_strm;
+
+    getline (myfile,line); // NCOLS
+    getline (myfile,line); // NROWS
+    getline (myfile,line); // XLLCORNER
+    getline (myfile,line); // YLLCORNER
+    getline (myfile,line); // CELLSIZE
+    (ds.dxy) =  getIntNumberFromString(line);
+    getline (myfile,line); // NODATA_VALUE
+    (ds.NODATA_VALUE) =  getIntNumberFromString(line);
+
+
+    // Read DEM from file and store in ds.zb
+    if (myfile.is_open()) //if the file is open
+    {
+        irow = 1;
+        while (! myfile.eof() ) //while the end of file is NOT reached
         {
-            for(irow=1;irow<=ds.NROWS;irow++)
-            {   
-                zbp = filedata(ds.NROWS - irow,icol-1);
-                zbn = filedata(ds.NROWS - irow,icol-1);
-                zbs = filedata(ds.NROWS - irow,icol-1);
-                zbe = filedata(ds.NROWS - irow,icol-1);
-                zbw = filedata(ds.NROWS - irow,icol-1);
-                zbp_corr = 0.0f;
-                if (zbp<0){ // check if zb=-99999 -> if yes, then it will behave as a weir
-                    (*ds.innerNeumannBCWeir).at(irow,icol) = 1.0f;
-                    if (zbn>0) zbp_corr = zbn;
-                    if (zbs>0) zbp_corr = (zbp_corr + zbs)/2;
-                    if (zbe>0) zbp_corr = 2/3*zbp_corr + 1/3*zbe;
-                    if (zbw>0) zbp_corr = 3/4*zbp_corr + 1/4*zbw;
-                    if (zbp_corr == 0.0f) zbp_corr = zbp;
-                }else
-                {
-                    (*ds.innerNeumannBCWeir).at(irow,icol) = 0.0f;
-                    zbp_corr = zbp;
+            std::stringstream str_strm;
+            getline (myfile,line); //get one line from the file
+            str_strm << line; //convert the string s into stringstream
+            
+            icol = 1;
+            while(!str_strm.eof()) {
+                str_strm >> temp_str; //take words into temp_str one by one
+                if(std::stringstream(temp_str) >> temp_float) { //try to convert string to int
+                    zb_raw.at(irow,icol) = temp_float;  // For now -99999 is set as 99999 to act like a wall using abs
+                    //std::cout << std::to_string(irow) + ", "+ std::to_string(icol) + ", "+ std::to_string(zb_raw.at(irow,icol)) << std::endl;
                 }
-                (*ds.zb).at(irow,icol) = std::abs(zbp_corr);  // For now -99999 is set as 99999 to act like a wall using abs
-                (*ds.ks).at(irow,icol) = ks_input; 
+                icol++;
+                temp_str = ""; //clear temp string
             }
+            str_strm.str(std::string());
+            irow++;
         }
-        //}
+        myfile.close(); //closing the file
         msg = "Successful loading of DEM file: " + ds.dem_file;
     } else{
         msg = "PROBLEM loading of DEM file: " + ds.dem_file;       
     } 
     std::cout << msg << std::endl;
     logFLUXOSfile << msg + "\n" ;
+
+
+    // Neumann condition for outer and NODATA_VALUE cells
+    for(icol=1;icol<=ds.NCOLS;icol++)
+    {
+        for(irow=1;irow<=ds.NROWS;irow++)
+        {   
+            zbp = zb_raw.at(irow,icol);
+
+            if (zbp == ds.NODATA_VALUE || zbp == 0.0f){ // check if NODATA_VALUE -> if yes, then it will behave as a weir
+               
+                zbn = zb_raw.at(irow+1,icol);
+                zbs = zb_raw.at(irow-1,icol);
+                zbe = zb_raw.at(irow,icol+1);
+                zbw = zb_raw.at(irow,icol-1);
+                zbp_corr = 0.0f;
+                n = 0;
+            
+                (*ds.innerNeumannBCWeir).at(irow,icol) = 1.0f;
+                if (zbn != ds.NODATA_VALUE && zbn != 0.0f && irow < ds.NROWS) {
+                    zbp_corr += zbn;
+                    n++;
+                }
+                if (zbs != ds.NODATA_VALUE && zbs != 0.0f && irow > 1) {
+                    zbp_corr += zbs;
+                    n++;
+                }
+                if (zbe != ds.NODATA_VALUE && zbe != 0.0f && icol < ds.NCOLS) {
+                    zbp_corr += zbe;
+                    n++;
+                }
+                if (zbw != ds.NODATA_VALUE && zbw != 0.0f && icol > 1) {
+                    zbp_corr += zbw;
+                    n++;
+                }
+                if (zbp_corr == 0.0f) {
+                    zbp_corr = zbp;
+                }else
+                {
+                    zbp_corr /= n;
+                }
+                
+            }else
+            {
+                (*ds.innerNeumannBCWeir).at(irow,icol) = 0.0f;
+                zbp_corr = zbp;
+            }
+            //zb_raw.at(irow,icol) = zbp_corr;
+            (*ds.zb).at(irow,icol) = zbp_corr;  // For now -99999 is set as 99999 to act like a wall using abs
+            (*ds.z).at(irow,icol) = zbp_corr;
+            (*ds.ks).at(irow,icol) = ks_input; 
+        }
+    }
 }
 
 float read_load(GlobVar& ds,std::ofstream& logFLUXOSfile)
