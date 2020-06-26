d2dd7fcbd758e030837d25ba39790eb562bed52d
DiogoCostaPT <1208buckas>
2020-06-26 17:03:00 -0600

    improvements to GeoMapping_results

--- fluxos_python/GeoMaping_results.m
+++ fluxos_python/GeoMaping_results.m
@@ -3,10 +3,10 @@ clear all
 
 sites = {'LON','ESS','RIS2','FW4','CMCDC2'};
 
-site_i = 1;
+site_i = 4;
 
 plot_options = {'dem_elevation','water_depth'};
-plot_i = 1;
+plot_i = 2;
 
 %% Input data
 
@@ -18,40 +18,55 @@ if (strcmp(site,'LON'))
     results_file = '/media/dcosta/data/megasync/my_server/fluxos/Janina_batch_2/LON/Results/151200.txt';
     delimiterIn = '\t';
     utm_zone = '17 T';
-    ix = 1;
-    iy = 2;
+    ix = 1; % for simulations
+    iy = 2; % for simulations
+    x_inverse = false;
+    row_i_north_utm = 3; % for DEM
+    row_i_east_utm = 4; % for DEM
 elseif (strcmp(site,'ESS'))
     % Essex
     demfile = '/media/dcosta/data/megasync/my_server/fluxos/Janina_batch_2/ESS/Essex_DEM_ascii_original';
     results_file = '/media/dcosta/data/megasync/my_server/fluxos/Janina_batch_2/ESS/Results/151200.txt';
     delimiterIn = ' ';
     utm_zone = '17 T';
-    ix = 2;
-    iy = 1;
+    ix = 2; % for simulations
+    iy = 1; % for simulations
+    x_inverse = false;
+    row_i_north_utm = 3; % for DEM
+    row_i_east_utm = 4; % for DEM
 elseif (strcmp(site,'RIS2'))
     % Essex
     demfile = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/RIS2/RIS2.asc';
     results_file = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/RIS2/Results/208800.txt';
     delimiterIn = ' ';
-    utm_zone = '14 U';
-    ix = 2;
-    iy = 1;
+    utm_zone = '14 T';
+    ix = 2; % for simulations
+    iy = 1; % for simulations
+    x_inverse = true;
+    row_i_north_utm = 4; % for DEM
+    row_i_east_utm = 3; % for DEM
 elseif (strcmp(site,'FW4'))
     % Essex
     demfile = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/FW4/FW4.asc';
     results_file = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/FW4/Results/259200.txt';
     delimiterIn = ' ';
     utm_zone = '14 U';
-    ix = 2;
-    iy = 1;
+    ix = 2; % for simulations
+    iy = 1; % for simulations
+    x_inverse = true;
+    row_i_north_utm = 4; % for DEM
+    row_i_east_utm = 3; % for DEM
 elseif (strcmp(site,'CMCDC2'))
     % Essex
     demfile = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/CMCDC2/CMCDC2.asc';
     results_file = '/media/dcosta/data/megasync/my_server/fluxos/Henry_Janina_3/CMCDC2/Results/90000.txt';
     delimiterIn = ' ';
     utm_zone = '14 U';
-    ix = 2;
-    iy = 1;
+    ix = 2; % for simulations
+    iy = 1; % for simulations
+    x_inverse = true;
+    row_i_north_utm = 4; % for DEM
+    row_i_east_utm = 3; % for DEM
 end
 
 cmax_waterdepth = 10; % in cm
@@ -69,34 +84,15 @@ b=regexp(dem_info,'\d+(\.)?(\d+)?','match');
 out=str2double([b{:}]);
 nrows = out(1);
 ncols = out(2);
-utm_north = out(3);
-utm_east = out(4);
+utm_north = out(row_i_north_utm);
+utm_east = out(row_i_east_utm);
 gridsize = out(5);
 NODATA_value = out(6);
 dem(dem == NODATA_value) = NaN;
 dem(dem == -NODATA_value) = NaN;
 
-% if plotting water depth
-if plot_i == 2
-
-    results_raw = importdata(results_file);
-    results_array = results_raw(:,1:4);
-    row_num = numel(results_array(:,1));
-    results_geoscatter = results_raw(:,[1:2,4]);
-
-    for rowi = 1:row_num
-        x = results_raw(rowi,ix);
-        y = results_raw(rowi,iy);
-
-        utm_east_i = utm_east + (x - 1)* gridsize;
-        utm_north_i = utm_north + (y - 1)* gridsize;
-
-        [lon, lat] = utm2deg(utm_east_i,utm_north_i,utm_zone);
-        results_geoscatter(rowi,1) = lon;
-        results_geoscatter(rowi,2) = lat;
-
-    end
-elseif plot_i == 1
+% for DEM
+if plot_i == 1
     dem_resh = reshape(dem,numel(dem),1);
     dem_array = zeros(numel(dem_resh),3)*NaN;
     dem_array(:,3) = dem_resh;
@@ -111,6 +107,29 @@ elseif plot_i == 1
             dem_array(linei,1) = lon;
             dem_array(linei,2) = lat;
         end
+    end
+% for water depth
+elseif plot_i == 2
+
+    results_raw = importdata(results_file);
+    results_array = results_raw(:,1:4);
+    row_num = numel(results_array(:,1));
+    results_geoscatter = results_raw(:,[1:2,4]);
+    
+
+    for rowi = 1:row_num
+        x = results_raw(rowi,ix);
+        y = results_raw(rowi,iy);
+        
+        if (x_inverse); y = ncols - y; end
+
+        utm_east_i = utm_east + (x - 1)* gridsize;
+        utm_north_i = utm_north + (y+1 - 1)* gridsize;
+
+        [lon, lat] = utm2deg(utm_east_i,utm_north_i,utm_zone);
+        results_geoscatter(rowi,1) = lon;
+        results_geoscatter(rowi,2) = lat;
+
     end
 end
 
@@ -129,6 +148,6 @@ elseif plot_i == 2
     set(get(hcb,'Title'),'String','Maximum water depth [cm]')   
 end
 geobasemap satellite
-alpha(gs1,1)
+alpha(gs1,0.5)
 title(['Site: ',site])
 
