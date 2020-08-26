0a67671c87651b6b3228e1ae1e1269cde4ab1f21
DiogoCostaPT <1208buckas>
2020-08-25 18:06:03 -0600

    Adding new data points for Janina's paper (in GeoMapping_results.m)

--- fluxos_python/GeoMaping_results.m
+++ fluxos_python/GeoMaping_results.m
@@ -1,15 +1,16 @@
 
 clear all
 
-sites = {'LON','ESS','RIS2','FW4','CMCDC2'};
+sites = {'LON','ESS','RIS2','FW4','CMCDC2','STC'};
 
 site_i = 5;
 
 plot_options = {'dem_elevation','water_depth'};
 plot_i = 2;
 
-cmax_waterdepth = 25; % in cm
+cmax_waterdepth = 305; % in cm
 add_sites = 1;
+MarkerFaceAlpha = 0.05;
 
 %% Input data
 
@@ -76,6 +77,18 @@ elseif (strcmp(site,'CMCDC2'))
     row_i_east_utm = 3; % for DEM
     sitesshapefile_up = '/media/dcosta/data/megasync/ec_main/models/fluxos/support/Janina_Henry_Merrin/CA_up.shp';
     sitesshapefile_down = '/media/dcosta/data/megasync/ec_main/models/fluxos/support/Janina_Henry_Merrin/CA_down.shp';
+elseif (strcmp(site,'STC'))
+    demfile = '/media/dcosta/data/megasync/ec_main/models/fluxos/support/STC_data_pre-processing/DEM_ASCII/steppler_dem_ascii_3m.txt';
+    results_file = '/media/dcosta/data/megasync/my_server/fluxos/batch_1_select_paper/t_65_paper/Results/723600.txt';
+    delimiterIn = ' ';
+    utm_zone = '14 U';
+    ix = 2; % for simulations
+    iy = 1; % for simulations
+    x_inverse = false;
+    row_i_north_utm = 4; % for DEM
+    row_i_east_utm = 3; % for DEM
+    %sitesshapefile_up = '/media/dcosta/data/megasync/ec_main/models/fluxos/support/Janina_Henry_Merrin/CA_up.shp';
+    %sitesshapefile_down = '/media/dcosta/data/megasync/ec_main/models/fluxos/support/Janina_Henry_Merrin/CA_down.shp';
 end
 
 
@@ -120,11 +133,12 @@ if plot_i == 1
 elseif plot_i == 2
 
     results_raw = importdata(results_file);
-    results_array = results_raw(:,1:4);
+    results_array = results_raw(:,1:end);
     row_num = numel(results_array(:,1));
-    results_geoscatter = results_raw(:,[1:2,4]);
+    results_geoscatter = results_raw(:,[1:2,end]);
+    small_loc = find(results_geoscatter(:,end)< 1);
+    results_geoscatter(small_loc,:) = NaN;
     
-
     for rowi = 1:row_num
         x = results_raw(rowi,ix);
         y = results_raw(rowi,iy);
@@ -156,7 +170,7 @@ elseif plot_i == 2
     set(get(hcb,'Title'),'String','Maximum water depth [cm]')   
 end
 geobasemap satellite
-alpha(gs1,0.1)
+alpha(gs1,MarkerFaceAlpha)
 title(['Site: ',site])
 
 % adding sites
@@ -178,3 +192,18 @@ if add_sites
    end
 end
 
+% To delete
+KVA = [0579921,5512270];
+KVB = [0579891,5513032];
+
+STM = [43+19/60,-81+07/60];
+lon = STM(1);
+lat = STM(2);
+
+ILD = [43+30/60,-74+23/60];
+lon = ILD(1);
+lat = ILD(2);
+
+[lon, lat] = utm2deg(KVB(1),KVB(2),'14 U');
+hold on
+geoscatter(lon,lat,70,styleform{f},'linewidth',1.5)
