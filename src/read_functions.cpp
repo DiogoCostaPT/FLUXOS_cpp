c4c793859776ecffc51e8cc3a1a02b25cf9c9f36
DiogoCostaPT <1208buckas>
2021-04-26 17:56:03 -0600

    big fix in processing of inflow_input discharge coordinates

--- src/read_functions.cpp
+++ src/read_functions.cpp
@@ -201,7 +201,7 @@ bool read_geo(
 
     // Determine discharge row and col
     if (ds.inflow_ycoord != NULL || ds.inflow_xcoord != NULL){
-        ds.inflow_nrow = ds.NROWS - std::round(ds.inflow_ycoord - ds.YLLCORNER)/ds.dxy; 
+        ds.inflow_nrow = std::round(ds.inflow_ycoord - ds.YLLCORNER)/ds.dxy; 
         ds.inflow_ncol = std::round(ds.inflow_xcoord - ds.XLLCORNER)/ds.dxy;
     }
 
