31b50fb0e6a6d9f38d55ca1f01b00eaa13bbd4a7
DiogoCostaPT <1208buckas>
2021-05-13 14:58:57 -0600

    add private variables to openMP (irow,cell_neumann)

--- src/hydrodynamics_calc.cpp
+++ src/hydrodynamics_calc.cpp
@@ -1,4 +1,3 @@
-
 // Copyright 1992: Cornel Beffa and Roland Faeh
 // Copyright 2013: Kashif Shaad and Diogo Costa
 // Copyright 2019, Diogo Costa
@@ -28,7 +27,7 @@
 void hydrodynamics_calc(
     GlobVar& ds)
 {
-           
+
 //-----------------------------------------------------------------------
 // Solves shallow water equation for one time-step - Ritter solver
 // Pressure term excluded from numerical flux
@@ -45,37 +44,40 @@ void hydrodynamics_calc(
     float cell_neumann;
 
     dtl = ds.dtfl;
-    
-    // GET hp AND CHECK IF DRY OR WET
-    for(icol=1;icol<=ds.NCOLS;icol++)
+
+    //Begin OpenMP parallel region
+    #pragma omp parallel
     {
-        for(irow=1;irow<=ds.NROWS;irow++)
-        {  
-            hp=std::fmax(0.0f,(*ds.z).at(irow,icol)-(*ds.zb).at(irow,icol));
-            (*ds.h).at(irow,icol) = hp;
-            
-            if(hp<=ds.hdry)
-            {
-              (*ds.qx).at(irow,icol)=0.0f;
-              (*ds.qy).at(irow,icol)=0.0f;
-              (*ds.us).at(irow,icol)=0.0f;
-              (*ds.ldry).at(irow,icol) = 1.0f;;          
-            } else
-            {
-              (*ds.ldry).at(irow,icol) = 0.0f;  
-              (*ds.twetimetracer).at(irow,icol) += dtl/3600; 
-            }
-        }
-    }
-    
-    // CALL FLOW SOLVERS (compute mass and momentum fluxes)
-     #pragma omp parallel
-    {
-        #pragma omp for collapse(2)
+
+        // GET hp AND CHECK IF DRY OR WET
+    #pragma omp for private(irow,hp)
         for(icol=1;icol<=ds.NCOLS;icol++)
         {
             for(irow=1;irow<=ds.NROWS;irow++)
-            {  
+            {
+                hp=std::fmax(0.0f,(*ds.z).at(irow,icol)-(*ds.zb).at(irow,icol));
+                (*ds.h).at(irow,icol) = hp;
+
+                if(hp<=ds.hdry)
+                {
+                  (*ds.qx).at(irow,icol)=0.0f;
+                  (*ds.qy).at(irow,icol)=0.0f;
+                  (*ds.us).at(irow,icol)=0.0f;
+                  (*ds.ldry).at(irow,icol) = 1.0f;;
+                } else
+                {
+                  (*ds.ldry).at(irow,icol) = 0.0f;
+                  (*ds.twetimetracer).at(irow,icol) += dtl/3600; 
+                }
+            }
+        }
+
+        // CALL FLOW SOLVERS (compute mass and momentum fluxes)
+    #pragma omp for private(irow,cell_neumann)
+        for(icol=1;icol<=ds.NCOLS;icol++)
+        {
+            for(irow=1;irow<=ds.NROWS;irow++)
+            {
                 cell_neumann = (*ds.innerNeumannBCWeir).at(irow,icol);
                 if((*ds.ldry).at(irow,icol) == 1.0f && cell_neumann == 0.0f) 
                 {
@@ -86,67 +88,70 @@ void hydrodynamics_calc(
                 }
             }
         }
-    }
-    
-    // CALCULATE TOTAL MASS AND MOMENTUM DERIVATIVE
-    for(icol=1;icol<=ds.NCOLS;icol++)
-    {
-        for(irow=1;irow<=ds.NROWS;irow++)
-        {  
-             cell_neumann = (*ds.innerNeumannBCWeir).at(irow,icol);
-             if (cell_neumann == 0.0f){
-                (*ds.dh).at(irow,icol) = 
-                    (((*ds.fe_1).at(irow-1,icol)-(*ds.fe_1).at(irow,icol))/ds.dxy 
-                    +((*ds.fn_1).at(irow,icol-1)-(*ds.fn_1).at(irow,icol))/ds.dxy)*dtl;
-                (*ds.dqx).at(irow,icol) = 
-                    (((*ds.fe_2).at(irow-1,icol)-(*ds.fe_2).at(irow,icol))/ds.dxy 
-                    +((*ds.fn_2).at(irow,icol-1)-(*ds.fn_2).at(irow,icol))/ds.dxy)*dtl;
-                (*ds.dqy).at(irow,icol) = 
-                    (((*ds.fe_3).at(irow-1,icol)-(*ds.fe_3).at(irow,icol))/ds.dxy 
-                    +((*ds.fn_3).at(irow,icol-1)-(*ds.fn_3).at(irow,icol))/ds.dxy)*dtl;
-                (*ds.qxf).at(irow,icol) = (*ds.fe_1).at(irow,icol)*dtl;
-                (*ds.qyf).at(irow,icol) = (*ds.fn_1).at(irow,icol)*dtl;
-             }else
-             {
-                (*ds.dh).at(irow,icol) = 0.0f;
-                (*ds.dqx).at(irow,icol) = 0.0f;
-                (*ds.dqy).at(irow,icol) = 0.0f;
-                (*ds.qxf).at(irow,icol) = 0.0f;
-                (*ds.qyf).at(irow,icol) = 0.0f;
-             }  
-        }
-    }
 
-    // CAL NEW VALUES
-    for(icol=1;icol<=ds.NCOLS;icol++)
-    {
-        for(irow=1;irow<=ds.NROWS;irow++)
-        {  
-            (*ds.z).at(irow,icol)=(*ds.z).at(irow,icol)+(*ds.dh).at(irow,icol);
-            hp=std::fmax(0.0f,(*ds.z).at(irow,icol)-(*ds.zb).at(irow,icol));
-            (*ds.h).at(irow,icol)=hp;
-            cell_neumann = (*ds.innerNeumannBCWeir).at(irow,icol);
-            
-            if(hp<ds.hdry || cell_neumann == 1.0f) 
+        // CALCULATE TOTAL MASS AND MOMENTUM DERIVATIVE
+    #pragma omp for private(irow,cell_neumann)
+        for(icol=1;icol<=ds.NCOLS;icol++)
+        {
+            for(irow=1;irow<=ds.NROWS;irow++)
             {
-                (*ds.qx).at(irow,icol)= 0.0f;
-                (*ds.qy).at(irow,icol)= 0.0f;
-                (*ds.us).at(irow,icol)= 0.0f;
-                (*ds.ldry).at(irow,icol) = 1.0f;
-            } else 
-            {
-                (*ds.qx).at(irow,icol)=
-                    (*ds.qx).at(irow,icol)+(*ds.dqx).at(irow,icol);  // numerical flux at cell center
-                (*ds.qy).at(irow,icol)=
-                    (*ds.qy).at(irow,icol)+(*ds.dqy).at(irow,icol);  // numerical flux at cell center
-                (*ds.qx).at(irow,icol)=
-                    .1*(*ds.qxf).at(irow-1,icol)+.8*(*ds.qx).at(irow,icol)
-                    +.1*(*ds.qxf).at(irow,icol); 
-                (*ds.qy).at(irow,icol)=
-                    .1*(*ds.qyf).at(irow,icol-1)+.8*(*ds.qy).at(irow,icol)
-                    +.1*(*ds.qyf).at(irow,icol);
-                (*ds.ldry).at(irow,icol) = 0.0f;          
-            }  
+                 cell_neumann = (*ds.innerNeumannBCWeir).at(irow,icol);
+                 if (cell_neumann == 0.0f){
+                    (*ds.dh).at(irow,icol) = 
+                        (((*ds.fe_1).at(irow-1,icol)-(*ds.fe_1).at(irow,icol))/ds.dxy 
+                        +((*ds.fn_1).at(irow,icol-1)-(*ds.fn_1).at(irow,icol))/ds.dxy)*dtl;
+                    (*ds.dqx).at(irow,icol) = 
+                        (((*ds.fe_2).at(irow-1,icol)-(*ds.fe_2).at(irow,icol))/ds.dxy 
+                        +((*ds.fn_2).at(irow,icol-1)-(*ds.fn_2).at(irow,icol))/ds.dxy)*dtl;
+                    (*ds.dqy).at(irow,icol) = 
+                        (((*ds.fe_3).at(irow-1,icol)-(*ds.fe_3).at(irow,icol))/ds.dxy 
+                        +((*ds.fn_3).at(irow,icol-1)-(*ds.fn_3).at(irow,icol))/ds.dxy)*dtl;
+                    (*ds.qxf).at(irow,icol) = (*ds.fe_1).at(irow,icol)*dtl;
+                    (*ds.qyf).at(irow,icol) = (*ds.fn_1).at(irow,icol)*dtl;
+                 }else
+                 {
+                    (*ds.dh).at(irow,icol) = 0.0f;
+                    (*ds.dqx).at(irow,icol) = 0.0f;
+                    (*ds.dqy).at(irow,icol) = 0.0f;
+                    (*ds.qxf).at(irow,icol) = 0.0f;
+                    (*ds.qyf).at(irow,icol) = 0.0f;
+                 }
+            }
         }
-    } 
+
+        // CAL NEW VALUES
+    #pragma omp for private(irow,cell_neumann,hp)
+        for(icol=1;icol<=ds.NCOLS;icol++)
+        {
+            for(irow=1;irow<=ds.NROWS;irow++)
+            {
+                (*ds.z).at(irow,icol)=(*ds.z).at(irow,icol)+(*ds.dh).at(irow,icol);
+                hp=std::fmax(0.0f,(*ds.z).at(irow,icol)-(*ds.zb).at(irow,icol));
+                (*ds.h).at(irow,icol)=hp;
+                cell_neumann = (*ds.innerNeumannBCWeir).at(irow,icol);
+
+                if(hp<ds.hdry || cell_neumann == 1.0f) 
+                {
+                    (*ds.qx).at(irow,icol)= 0.0f;
+                    (*ds.qy).at(irow,icol)= 0.0f;
+                    (*ds.us).at(irow,icol)= 0.0f;
+                    (*ds.ldry).at(irow,icol) = 1.0f;
+                } else 
+                {
+                    (*ds.qx).at(irow,icol)=
+                        (*ds.qx).at(irow,icol)+(*ds.dqx).at(irow,icol);  // numerical flux at cell center
+                    (*ds.qy).at(irow,icol)=
+                        (*ds.qy).at(irow,icol)+(*ds.dqy).at(irow,icol);  // numerical flux at cell center
+                    (*ds.qx).at(irow,icol)=
+                        .1*(*ds.qxf).at(irow-1,icol)+.8*(*ds.qx).at(irow,icol)
+                        +.1*(*ds.qxf).at(irow,icol); 
+                    (*ds.qy).at(irow,icol)=
+                        .1*(*ds.qyf).at(irow,icol-1)+.8*(*ds.qy).at(irow,icol)
+                        +.1*(*ds.qyf).at(irow,icol);
+                    (*ds.ldry).at(irow,icol) = 0.0f;
+                }
+            }
+        } 
+
+    } //end of OpenMP parallel region
 }
